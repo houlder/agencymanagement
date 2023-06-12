57e7c0c069ed1526da9480c3d2304cc81cb68424
houlder <houlder01@yahoo.fr>
2023-06-12 15:55:47 +0300

    refactor : add status code to error handler function

--- core/views.py
+++ core/views.py
@@ -3,19 +3,19 @@ from django.shortcuts import render
 
 def handler400(request, exception):
     """handler exception when error 400 case"""
-    return render(request, "errors/400.html")
+    return render(request, "errors/400.html", status=400)
 
 
 def handler403(request, exception):
     """handler exception when error 403 case"""
-    return render(request, "errors/403.html")
+    return render(request, "errors/403.html", status=403)
 
 
 def handler404(request, exception):
     """handler exception when error 404 case"""
-    return render(request, "errors/404.html")
+    return render(request, "errors/404.html", status=404)
 
 
 def handler500(request):
     """handler exception when error 500 case"""
-    return render(request, "errors/500.html")
+    return render(request, "errors/500.html", status=500)
